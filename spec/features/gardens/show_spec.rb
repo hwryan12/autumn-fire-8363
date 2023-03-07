585d8a1234386c5cd4a2e048088ef66e0c75b564
Harrison Ryan <hwryan12@gmail.com>
2023-03-07 11:00:30 -0600

    features: Test setup for us 3

---
+++ spec/features/gardens/show_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Gardens Show" do
+  let!(:garden) { Garden.create(name: "Turing Community Garden", organic: true) }
+
+  let!(:plot_1) { garden.plots.create(number: 1, size: "Large", direction: "East") }
+  let!(:plot_2) { garden.plots.create(number: 2, size: "Large", direction: "North") }
+  let!(:plot_3) { garden.plots.create(number: 3, size: "Medium", direction: "East") }
+
+  let!(:plant_1) { Plant.create(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90) }
+  let!(:plant_2) { Plant.create(name: "Gold Potatoes", description: "Just stick it in the ground.", days_to_harvest: 120) }
+  let!(:plant_3) { Plant.create(name: "Basil", description: "Prefers indirect sun.", days_to_harvest: 60) }
+  let!(:plant_4) { Plant.create(name: "Sunflowers", description: "Prefers lots and lots of sun.", days_to_harvest: 100) }
+
+  before do 
+    plot_1.plot_plants.create!(plant: plant_1)
+    plot_1.plot_plants.create!(plant: plant_2)
+    plot_1.plot_plants.create!(plant: plant_3)
+    plot_2.plot_plants.create!(plant: plant_2)
+    plot_3.plot_plants.create!(plant: plant_3)
+    plot_3.plot_plants.create!(plant: plant_4)
+
+    visit "/gardens/#{garden.id}"
+  end 
+
+  describe "User Story 3" do
+    context "As a visitor" do
+      context "When I visit a garden's show page" do
+        it "Then I see a list of plants that are included in that garden's plots
+          And I see that this list is unique (no duplicate plants)
+          And I see that this list only includes plants that take less than 100 days to harvest" do
+          
+          expect(page).to have_content("Plant Name: Gold Potatoes").once
+          expect(page).to have_content("Description: Just stick it in the ground.").once
+          expect(page).to have_content("Days until ready to harvest: 120").once
+          
+          expect(page).to have_content("Plant Name: Sunflowers").once
+          expect(page).to have_content("Description: Prefers lots and lots of sun.").once
+          expect(page).to have_content("Days until ready to harvest: 100").once
+          
+          expect(page).to_not have_content("Plant Name: Purple Beauty Sweet Bell Pepper")
+          expect(page).to_not have_content("Description: Prefers rich, well draining soil")
+          expect(page).to_not have_content("Days until ready to harvest: 90")
+
+          expect(page).to_not have_content("Plant Name: Basil")
+          expect(page).to_not have_content("Description: Prefers indirect sun.")
+          expect(page).to_not have_content("Days until ready to harvest: 60")
+        end
+      end
+    end
+  end
+end
